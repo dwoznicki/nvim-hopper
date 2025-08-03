237d4bacdaa5db914b194eaed55afc8cee7e7db5
Daniel Woznicki <daniel.woznicki@gmail.com>
2025-08-02 23:04:58 -0700

    Flipped default keymap length back to 2. 3 was just for testing.

--- lua/hopper/options.lua
+++ lua/hopper/options.lua
@@ -19,11 +19,8 @@ local M = {}
 
 local _default_options = { ---@type hopper.ResolvedOptions
   keymapping = {
-    -- keyset = {
-    --   "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
-    -- },
     keyset = keymaps.keysets.alphanumeric,
-    length = 3,
+    length = 2,
   },
 }
 
